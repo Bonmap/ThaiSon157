1996d396562b30385595698d6cc413af7e9e352d
bonmap <son2nkt@gmail.com>
2023-08-19 16:43:04 +0700

    0.3

---
+++ HelloWorld/Hello World.cs
@@ -0,0 +1,14 @@
+﻿
+namespace HelloWorld
+{
+   class Program
+   {
+        static void Main(string[] args)
+        {
+            Console.WriteLine ("Hello world");
+            Console.WriteLine ("Hello Nguyen Khac Thai Son");
+        }
+   } 
+    
+}
+
