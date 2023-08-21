6aa5938b127d15840d22ed7d28cbe27fbd79eaab
bonmap <son2nkt@gmail.com>
2023-08-21 12:32:07 +0700

    Bai tap 1

---
+++ Bai tap 1/Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Systemtime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime localDate = DateTime.Now;
+            System.Console.WriteLine("Datetime Now is :" + localDate);
+        } 
+     }
+}
