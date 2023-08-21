fa71fef01cf27dbbfdb9deebba69086ba574a4a0
bonmap <son2nkt@gmail.com>
2023-08-21 14:16:41 +0700

    chuyen doi tien te

---
+++ Chuyen doi tien te/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace chuyendoitiente
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            float USD;
+            float VND = 23000f;
+            Console.Write("Enter USD: ");
+            USD = float.Parse(Console.ReadLine());
+            float area = USD*VND;
+            Console.WriteLine(area);
+        }
+        
+    }
+}
