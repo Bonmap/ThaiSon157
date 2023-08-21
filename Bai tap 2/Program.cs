030ec30405bfa52f6a895936a469c72a043dbbb5
bonmap <son2nkt@gmail.com>
2023-08-21 13:36:12 +0700

    Su dung toan tu

---
+++ Bai tap 2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace sudungtoantu
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            float w;
+            float h;
+            Console.Write("Enter width: ");
+            w = float.Parse(Console.ReadLine());
+            Console.Write("Enter height: ");
+            h = float.Parse(Console.ReadLine());
+            float area = w*h;
+            Console.WriteLine(area);
+        }
+        
+    }
+}
