67decbe26baaba252bf0ee82c3a5242610c85034
bonmap <son2nkt@gmail.com>
2023-08-22 22:58:37 +0700

    Chuyen doi tien te 2

---
+++ Chuyen doi tien te 2/Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace cannangcuacothe
+{
+    class Program
+    {
+        const int VND_RATE=23000;
+        static void Main(string[] args)
+        { 
+            int usd;
+            Console.WriteLine("Nhap vao bao nhieu USD");
+            usd = int.Parse(Console.ReadLine());
+            int ketqua = usd * VND_RATE;
+            Console.WriteLine("Ket qua la "+ ketqua + " VND");
+        }
+
+    }
+}
