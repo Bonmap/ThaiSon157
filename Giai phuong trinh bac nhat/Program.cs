9dc023beaa7cba73a9b818cb9c169b3338118f90
bonmap <son2nkt@gmail.com>
2023-08-23 19:07:47 +0700

    Giai phuong trinh bac nhat

---
+++ Giai phuong trinh bac nhat/Program.cs
@@ -0,0 +1,31 @@
+﻿
+public class Program
+{
+    static void Main(string[] args)
+    {
+        double a;
+        double b;
+        double c;
+        Console.WriteLine("Linear Equation Resolver");
+        Console.WriteLine("Given a equation as 'a * x + b = 0', please enter constants:");
+        Console.Write("so a la bao nhieu: ");
+        a = Convert.ToDouble(Console.ReadLine());
+        Console.Write("so b la bao nhieu: ");
+        b = Convert.ToDouble(Console.ReadLine());
+        Console.Write("so c la bao nhieu: ");
+        c = Convert.ToDouble(Console.ReadLine());
+        if (b == 0)
+        {
+            if (a == c)
+            { Console.WriteLine("Phuong trinh vo so nghiem"); }
+            else
+            { Console.WriteLine("Phuong trinh vo nghiem"); }
+        }
+        else
+        {
+            double x = (a - c) / b;
+            Console.WriteLine("Dap an cua phuong trinh " + x);
+        }
+    }
+
+}
