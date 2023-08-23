33bc9816bd69ffac8e5917f521dc9c587ac3ee9c
bonmap <son2nkt@gmail.com>
2023-08-23 11:21:18 +0700

    Dieu kien uong bia

---
+++ Dieu kien uong bia/Program.cs
@@ -0,0 +1,17 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        int tuoi;
+        Console.WriteLine("Ban bao nhieu tuoi");
+        tuoi = int.Parse(Console.ReadLine());
+        if (tuoi >= 18)
+        {
+            Console.WriteLine("Du tuoi uong bia");
+        }
+        else
+        {
+            Console.WriteLine("Khong du tuoi uong bia");
+        }
+    }
+}
