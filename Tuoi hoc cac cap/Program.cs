b4fe3c100a1f27e0e11eee4c314c94a32649f637
bonmap <son2nkt@gmail.com>
2023-08-23 12:06:13 +0700

    Tuoi hoc cac cap

---
+++ Tuoi hoc cac cap/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int tuoi;
+        Console.WriteLine("Ban bao nhieu tuoi");
+        tuoi = int.Parse(Console.ReadLine());
+        if (tuoi >= 6 && tuoi <= 10)
+        {
+            Console.WriteLine("Dang hoc tieu hoc");
+        }
+        else if (tuoi >= 11 && tuoi <= 14)
+        {
+            Console.WriteLine("Dang hoc THCS");
+        }
+        else if (tuoi >= 15 && tuoi <= 18)
+        {
+            Console.WriteLine("Dang hoc THPT");
+        }   
+        else
+        {
+            Console.WriteLine("Da ra truong");
+        }   
+
+    }
+}
