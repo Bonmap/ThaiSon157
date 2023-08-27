198a2cfa28c5954859d78f21998b1b779beab973
bonmap <son2nkt@gmail.com>
2023-08-27 21:48:19 +0700

    Tim so nguyen to

---
+++ Tim so nguyen to/Program.cs
@@ -0,0 +1,25 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        int a;
+        Console.WriteLine("Tim so nguyen to");
+        Console.WriteLine("Vui long nhap so");
+        a = Convert.ToInt32(Console.ReadLine());
+        bool so_nguyen_to = true;
+        for (int i = 2; i < a; i++)
+            if (a % i == 0)
+            {
+                so_nguyen_to = false;
+            }
+        switch (so_nguyen_to)
+        {
+            case true:
+                Console.WriteLine("So a la so nguyen to");
+                break;
+            case false:
+                Console.WriteLine("So a khong phai la so nguyen to");
+                break;
+        }
+    }
+}
