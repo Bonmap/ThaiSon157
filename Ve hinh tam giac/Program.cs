6e49cd2caaa3c3b69a7b636903b179a3ea44ece0
bonmap <son2nkt@gmail.com>
2023-08-28 18:13:29 +0700

    Ve hinh tam giac

---
+++ Ve hinh tam giac/Program.cs
@@ -0,0 +1,20 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        int chieu_dai_canh = 1;
+        Console.WriteLine("Ve hinh tam giac can");
+        Console.Write("Nhap chieu dai canh tam giac: ");
+        chieu_dai_canh = Convert.ToInt32(Console.ReadLine());
+        int CDC_tam_thoi = chieu_dai_canh;
+        for (int a = 0; a < chieu_dai_canh; a++)
+        {
+            for (int b = 0; b < CDC_tam_thoi; b++)
+            {
+                Console.Write("*");
+            }
+            CDC_tam_thoi--;
+            Console.WriteLine("");
+        }
+    }
+}
