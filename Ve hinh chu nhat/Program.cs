541a5d23eacfcacaa0a1aacbea995341f06beec3
bonmap <son2nkt@gmail.com>
2023-08-28 12:33:45 +0700

    Ve hinh chu nhat

---
+++ Ve hinh chu nhat/Program.cs
@@ -0,0 +1,20 @@
+﻿class program
+{
+    public static void Main(string[] args)
+    {
+        int chieu_dai;
+        int chieu_rong;
+        Console.WriteLine("Ve hinh chu nhat");
+        Console.WriteLine("chieu dai hinh chu nhat");
+        chieu_dai = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("chieu rong hinh chu nhat");
+        chieu_rong = Convert.ToInt32(Console.ReadLine());
+        for (int j = 0; j < chieu_rong; j++)
+        {    for (int i = 0; i < chieu_dai; i++)
+            {
+                Console.Write("o");
+            }
+            Console.WriteLine("");
+        }    
+    }
+}
