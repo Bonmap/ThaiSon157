8199350bf6a286a5105fc6f4a6c1cbd4e8bfd6ef
bonmap <son2nkt@gmail.com>
2023-09-05 00:35:52 +0700

    Tinh tong cac phan tu trong mang

---
+++ Tinh tong cac phan tu trong mang/Program.cs
@@ -0,0 +1,18 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+       int [] numbers= new int [5];
+       numbers[0]=1;
+       numbers[1]=2;
+       numbers[2]=3;
+       numbers[3]=4;
+       numbers[4]=5;
+       int total = 0;
+       for(int i=0;i<numbers.Length;i++)
+       {
+        total=numbers[i]+total;
+       }
+      Console.WriteLine(total);
+    }
+}
