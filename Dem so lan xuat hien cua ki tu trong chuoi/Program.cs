951b5cfe120205e543aab17e910d1e3ecf7892f8
bonmap <son2nkt@gmail.com>
2023-09-13 16:28:01 +0700

    Dem so lan xuat hien ki tu trong chuoi

---
+++ Dem so lan xuat hien cua ki tu trong chuoi/Program.cs
@@ -0,0 +1,32 @@
+﻿class program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Dem so lan xuat hien cua ki tu trong chuoi");
+        Console.WriteLine("Nhap chuoi");
+        string chuoi = Console.ReadLine();
+        Console.WriteLine("Nhap ki tu");
+        char kitutim = Convert.ToChar(Console.ReadLine());
+        if (Demsolanxuathienkytu(chuoi, kitutim) == 0)
+        {
+            Console.Write("ki tu khong co trong chuoi");
+        }
+        else
+        {
+            Console.Write("So lan xuat hien la: ");
+            Console.WriteLine(Demsolanxuathienkytu(chuoi, kitutim));
+        }
+    }
+    public static int Demsolanxuathienkytu(string chuoi, char kitutim)
+    {
+        int dem = 0;
+        for (int i = 0; i < chuoi.Length; i++)
+        {
+            if (chuoi[i] == kitutim)
+            {
+                dem++;
+            }
+        }
+        return dem;
+    }
+}
